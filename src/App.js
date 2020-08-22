75c48a2b777468198e10c728909b2f4dda34ad61
Ivy <ivyla3@gmail.com>
2020-08-22 16:09:44 -0700

    updated README and added routing + set up the components for each page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Experience from "./components/Experience"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        {/* <header> */}
+        <Link to="/home">
+          home
+          </Link>
+        <Link to="/about">
+          about
+          </Link>
+        <Link to="/projects">
+          projects
+          </Link>
+        <Link to="/experience">
+          experience
+          </Link>
+        {/* </header> */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/experience">
+            <Experience />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
