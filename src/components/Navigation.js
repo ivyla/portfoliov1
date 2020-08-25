cf7914ae32e642d1e19881706a3c70e66c637858
Ivy <ivyla3@gmail.com>
2020-08-25 00:44:29 -0700

    ok i somwhat got a hamburger menu working on my owngit add -A just need to fix up the styling. yayyy i did it :')

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,6 +12,8 @@ import Projects from "./Projects"
 import Experience from "./Experience"
 import "../stylesheets/Navigation.css"
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { MenuOutlined } from '@ant-design/icons'
+
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Next time - work on getting a normal navbar to work, and then try to use it 
@@ -20,42 +22,69 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 // https://react-bootstrap.github.io/components/navbar/#navbar-props
 // https://stackoverflow.com/questions/48330233/react-router-bootstrap-and-navbar-redirection
 // https://github.com/react-bootstrap/react-router-bootstrap
-function Navigation() {
-    return (
-        <Router>
-            <div className="navbar">
-                <div className="navbar-content">
-                    {/* <header> */}
-                    <NavLink className="navItem" to="/">
-                        home
-                    </NavLink>
-                    <NavLink className="navItem" to="/about">
-                        about
-                    </NavLink>
-                    <NavLink className="navItem" to="/projects">
-                        projects
-                    </NavLink>
-                    <NavLink className="navItem" to="/experience">
-                        experience
-                    </NavLink>
-                </div>
+class Navigation extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            menuPressed: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState((prevState) => ({ menuPressed: !prevState.menuPressed }))
+        console.log("clicked!" + this.state.menuPressed);
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    {/* <div className="navbar"> */}
+                    <div className={this.state.menuPressed ? "navbar-large" : "navbar"}>
+                        {/* <div className="navbar-content"> */}
+                        <div className={this.state.menuPressed ? "navbar-content-mobile" : "navbar-content"}>
+                            {/* <header> */}
+                            <NavLink className="navItem" to="/">
+                                home
+                        </NavLink>
+                            <NavLink className="navItem" to="/about">
+                                about
+                        </NavLink>
+                            <NavLink className="navItem" to="/projects">
+                                projects
+                        </NavLink>
+                            <NavLink className="navItem" to="/experience">
+                                experience
+                        </NavLink>
+                        </div>
+
+                        <div className="hamburger-menu">
+                            <a href="#" onClick={this.handleClick}>
+                                <MenuOutlined style={{ color: 'white', float: 'right' }} />
+                            </a>
+                        </div>
+                    </div>
+                    {/* </header> */}
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/projects">
+                            <Projects />
+                        </Route>
+                        <Route path="/experience">
+                            <Experience />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router >
             </div>
-            {/* </header> */}
-            <Switch>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/projects">
-                    <Projects />
-                </Route>
-                <Route path="/experience">
-                    <Experience />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
-    )
+        )
+
+    }
 }
 export default Navigation
