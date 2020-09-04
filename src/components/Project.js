3c96813a01fe63d7bfa12481490f51d780df130a
Ivy <ivyla3@gmail.com>
2020-09-04 14:42:05 -0700

    projects lookin ugly rn but ill fix it next time

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,30 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import "../stylesheets/Theme.css"
+import "../stylesheets/Projects.css"
+
+import { LinkOutlined } from "@ant-design/icons"
 
 function Project(props) {
     return (
         <div>
-            {/* Project Name */}
-            <p>Project name: {props.project.name} </p>
-            {/* Project link?  */}
-            <p>Project link : </p>
+            <div className="project-content">
+                {/* Project Name */}
+                <div className="default-header"> {props.project.name}
+                    {/* Project link?  */}
+                    {/* <a href={props.project.link} target="_blank" rel="noopener noreferrer" > <LinkOutlined /> </a> */}
 
-            {/* Active Data */}
-            <p>Active Dates: </p>
+                    {/* Active Data */}
+                    {props.project.startDate} - {props.project.endDate}
+                </div>
 
-            {/* Technologies */}
-            <p>Technologies : </p>
+                {/* Technologies */}
+                <p>Technologies: {props.project.technologies} </p>
 
-            {/* Brief project description */}
-            <p>Project description: </p>
+                {/* Brief project description */}
+                <p> {props.project.desc} </p>
+            </div>
 
-            <p>-----------------------</p>
         </div>
     )
 }
