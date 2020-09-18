a65afc221ab3932ea524a712d99e4acea4b59e12
Ivy <ivyla3@gmail.com>
2020-09-17 21:44:37 -0700

    added favicon and react-helmet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import Navigation from './components/Navigation';
+import { Helmet } from "react-helmet";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import favicon from "./assets/favicon.ico"
 
 function App() {
   return (
     <div className="App">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Helmet>
+        <title> ivyla.me </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" href={favicon} />
+      </Helmet>
       <Navigation />
     </div >
   );
