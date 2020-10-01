e95c0b52dfef8fa64659e23d3f6a46a368cffef7
Ivy <ivyla3@gmail.com>
2020-10-01 13:47:09 -0700

    made progress on the smallest media query for the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,53 +7,56 @@ import { CSSTransition, TransitionGroup } from "react-transition-group"
 
 function Home() {
     return (
-        <div className="transition-wrapper">
 
-            <div className="wrapper">
+        <div className="wrapper-home">
 
-                <div className="content-home">
-                    <div>
-                        <p className="default-title-center"> Hi, I'm Ivy. </p>
-                    </div>
+            <div className="content-home">
+                <div className="default-title">
+                    Hi, I'm Ivy.
+                </div>
 
-                    <div>
-                        <p className="default-header-center"> I like writing code & designing. </p>
-
-                    </div>
-
-                    <div >
-
-                        <p className="default-description-center">
-                            Nam ut dui nec ligula sagittis pharetra et sit
-                            amet turpis. Cras malesuada, nulla ut convalli
-                            maximus, metus mauris tempus elit, asceler
-                            isque mauris turpis sit amet tortor. Proin effic
-                pretium dolor at finibus. Morbi ultricies  </p>
-                    </div>
-
-                    <div className="media-icons-container">
-                        <a href="https://linkedin.com/in/ivyla" target="_blank" rel="noopener noreferrer">
-                            <LinkedinFilled className="media-icons" id="linkedin-icon" />
-                        </a>
-                        <a href="https://github.com/ivyla" target="_blank" rel="noopener noreferrer">
-                            <GithubFilled className="media-icons" id="github-icon" />
-                        </a>
-
-                        <a href="mailto:ila@ucsc.edu" target="_blank" rel="noopener noreferrer">
-                            <MailFilled className="media-icons" id="email-icon" />
-                        </a>
-
-                        <a href="https://www.instagram.com/stegoivysaur/" target="_blank" rel="noopener noreferrer">
-                            <InstagramFilled className="media-icons" id="instagram-icon" />
-                        </a>
-
-                    </div>
-                    {/* linkedin github email resume */}
+                <div className="default-header">
+                    I like writing code & designing.
 
                 </div>
 
-            </div >
-        </div>
+                <div className="default-desc-home">
+                    Nam ut dui nec ligula sagittis pharetra et sit
+                    amet turpis. Cras malesuada, nulla ut convalli
+                    maximus, metus mauris tempus elit, asceler
+                    isque mauris turpis sit amet tortor. Proin effic
+                    pretium dolor at finibus. Morbi ultricies. Proin effic
+                    pretium dolor at finibus. Morbi ultricies.
+                    {/* I'm a software developer currently focusing
+                    on growing my skills in front end and fullstack technologies.
+                    I strive to create engaging and meaningful
+                    user experiences through software engineering
+                    and front end design; bringing any ideas to life
+                    from paper to screen. */}
+                </div>
+
+                <div className="media-icons-container">
+                    <a href="https://linkedin.com/in/ivyla" target="_blank" rel="noopener noreferrer">
+                        <LinkedinFilled className="media-icons" id="linkedin-icon" />
+                    </a>
+                    <a href="https://github.com/ivyla" target="_blank" rel="noopener noreferrer">
+                        <GithubFilled className="media-icons" id="github-icon" />
+                    </a>
+
+                    <a href="mailto:ila@ucsc.edu" target="_blank" rel="noopener noreferrer">
+                        <MailFilled className="media-icons" id="email-icon" />
+                    </a>
+
+                    <a href="https://www.instagram.com/stegoivysaur/" target="_blank" rel="noopener noreferrer">
+                        <InstagramFilled className="media-icons" id="instagram-icon" />
+                    </a>
+
+                </div>
+                {/* linkedin github email resume */}
+
+            </div>
+
+        </div >
     )
 }
 
