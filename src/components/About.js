26e3ca095664d967d9cd6d4d91de3f9e429ad3d5
Ivy <ivyla3@gmail.com>
2020-10-02 14:41:28 -0700

    finished about me mobile

--- src/components/About.js
+++ src/components/About.js
@@ -9,21 +9,21 @@ function About() {
         <div className="transition-wrapper">
             <div className="wrapper-about">
                 <div className="content-about">
-                    <div className="name-and-desc-about">
-                        <div className="default-title">
-                            About Me
+                    {/* <div className="name-and-desc-about"> */}
+                    <div className="default-title">
+                        About Me
                         </div>
-                        <div className="default-desc">
-                            Nam ut dui nec ligula sagittis pharetra et sit
-                            amet turpis. Cras malesuada, nulla ut convalli
-                            maximus, metus mauris tempus elit, ascelerd
-                            isque mauris turpis sit amet tortor. Proin effics
-                            pretium dolor at finibus. Morbi ultricies posues
-                            elit, asceler isque mauris turpis sit amet tortor.
-                            Proin effic pretium dolor at finibus. Morbi ultric.
+                    <div className="default-desc">
+                        Nam ut dui nec ligula sagittis pharetra et sit
+                        amet turpis. Cras malesuada, nulla ut convalli
+                        maximus, metus mauris tempus elit, ascelerd
+                        isque mauris turpis sit amet tortor. Proin effics
+                        pretium dolor at finibus. Morbi ultricies posues
+                        elit, asceler isque mauris turpis sit amet tortor.
+                        Proin effic pretium dolor at finibus. Morbi ultric.
                         </div>
 
-                    </div>
+                    {/* </div> */}
 
                     <div className="about-image">
                         <img src={portrait} />
