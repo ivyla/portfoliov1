43c9a385642cdb0c1b8946d034330e788a895273
Ivy <ivyla3@gmail.com>
2020-10-09 20:48:31 -0700

    Updates

---
+++ precache-manifest.0b33ccab535429fc47cfb2ffda8e03eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae88223e945b0f875e07aa3ea66448a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966efca6968db8aedd95",
+    "url": "/static/css/main.ef752338.chunk.css"
+  },
+  {
+    "revision": "8e704b83bb10f03171ca",
+    "url": "/static/js/2.5e5f465b.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/static/js/2.5e5f465b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966efca6968db8aedd95",
+    "url": "/static/js/main.00c76a1f.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "8dca98dd50b2ba1e63ae514552de5490",
+    "url": "/static/media/191116.8dca98dd.jpg"
+  },
+  {
+    "revision": "f372ddd546b90a841d482ddb3bd700ed",
+    "url": "/static/media/favicon.f372ddd5.ico"
+  },
+  {
+    "revision": "d8450c0bad86ec78f812266e8af81e68",
+    "url": "/static/media/location.d8450c0b.svg"
+  },
+  {
+    "revision": "54cebee352a2ddf98087c8d17b65e9d2",
+    "url": "/static/media/logo-main.54cebee3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a40e5dce67b90b887f35b86417e28ec",
+    "url": "/static/media/portrait.7a40e5dc.jpg"
+  }
+]);
