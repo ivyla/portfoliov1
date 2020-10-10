e7ba90c5272af69303c98cf1c00226c69ed5cd74
Ivy <ivyla3@gmail.com>
2020-10-09 21:21:38 -0700

    Updates

---
+++ precache-manifest.3f7be3865d8a3dabf45c5d3194bb3a17.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a450e87d563a4d6759f90abe08b12c52",
+    "url": "/ivyla.github.io/index.html"
+  },
+  {
+    "revision": "2540fec253021d2d9e22",
+    "url": "/ivyla.github.io/static/css/main.ef9e3096.chunk.css"
+  },
+  {
+    "revision": "e8c2034ef0789878efb9",
+    "url": "/ivyla.github.io/static/js/2.5ce0e359.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/ivyla.github.io/static/js/2.5ce0e359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2540fec253021d2d9e22",
+    "url": "/ivyla.github.io/static/js/main.e9ed82f1.chunk.js"
+  },
+  {
+    "revision": "6e2d85f8a6df041aa9dd",
+    "url": "/ivyla.github.io/static/js/runtime-main.9609f18d.js"
+  },
+  {
+    "revision": "8dca98dd50b2ba1e63ae514552de5490",
+    "url": "/ivyla.github.io/static/media/191116.8dca98dd.jpg"
+  },
+  {
+    "revision": "f372ddd546b90a841d482ddb3bd700ed",
+    "url": "/ivyla.github.io/static/media/favicon.f372ddd5.ico"
+  },
+  {
+    "revision": "d8450c0bad86ec78f812266e8af81e68",
+    "url": "/ivyla.github.io/static/media/location.d8450c0b.svg"
+  },
+  {
+    "revision": "54cebee352a2ddf98087c8d17b65e9d2",
+    "url": "/ivyla.github.io/static/media/logo-main.54cebee3.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ivyla.github.io/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "7a40e5dce67b90b887f35b86417e28ec",
+    "url": "/ivyla.github.io/static/media/portrait.7a40e5dc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b33ccab535429fc47cfb2ffda8e03eb.js"
+  "/ivyla.github.io/precache-manifest.3f7be3865d8a3dabf45c5d3194bb3a17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ivyla.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
