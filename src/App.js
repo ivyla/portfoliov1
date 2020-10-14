18c636120705eb65b58c0e648be9b72b5fc20546
Ivy <ivyla3@gmail.com>
2020-10-14 00:14:41 -0700

    added CNAME into package lock

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <title> ivyla.me </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href={favicon} />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet"/>      
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;0,900;1,400&display=swap" rel="stylesheet"/>      
       </Helmet>
       <Navigation />
     </div >
